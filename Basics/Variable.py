724ecc8ba51d2d19e3f18fa85810ff6e8bf824a4
Prabha1909 <prabha.svm@gmail.com>
2018-10-25 23:04:45 +0530

    Variable file created and worked on it

---
+++ Basics/Variable.py
@@ -0,0 +1,8 @@
+#Variable is nothing but that hold the object or datatypes
+#Rules for writing a variable
+#It can start with alphabet and underscore
+#It cannot start with the numbers however we can have numbers in between
+#It should always be written in small letters as it is the standard that is followed
+
+greetings = "Hello" #greeting is a variable
+print(greetings)
