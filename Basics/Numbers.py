2a0290906999552a384add2f07f4bd4e03627089
Prabha1909 <prabha.svm@gmail.com>
2018-10-27 22:00:33 +0530

    Created number.py and worked on it

---
+++ Basics/Numbers.py
@@ -0,0 +1,5 @@
+a = 10 #this is anumber assigned to a variable a
+print(100 + 100) #we can add 2 numbers
+#There are two types of numbers
+print(type(100)) #type integer
+print(type(100.0)) #type float
