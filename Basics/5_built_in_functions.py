042b7c37533a256b2f55185f96172c0a4bf7b5d8
Prabha1909 <prabha.svm@gmail.com>
2018-10-27 22:25:39 +0530

    float, int on numbers used as strings

--- Basics/5_built_in_functions.py
+++ Basics/5_built_in_functions.py
@@ -5,3 +5,7 @@ print(dir(__builtins__))
 print(int(10.5)) #it ignores the value after the decimal
 #round function round of the value 10.6 to 11
 print(round(10.6))
+#float to retur  the float values to integers
+print(float(100))
+#Also, we can use int function in string to return the int values - applicable on number used as string
+print(int("100") + 100) # return 200 rather than an error
