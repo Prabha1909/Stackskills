42903f11d1adacd45a1aeab1309e64f233cd182d
Prabha1909 <prabha.svm@gmail.com>
2018-10-27 21:36:46 +0530

    worked on strings.py

--- Basics/Strings.py
+++ Basics/Strings.py
@@ -4,3 +4,13 @@ print("Hello")
 print(type("Hello"))
 #We can even print the length of the string using len function
 print(len("Hello"))
+#we can assign string to a variabel and print it
+my_string = "Hello world"
+print(my_string)
+#we can concatenate the string
+print("100" + "100")
+#we can concatenate string with other object types
+#print([1, 2] + "100") This produces an error hence commented it out
+#we can index and slice the string
+print(my_string[0]) # prints the first letter of the string
+print(my_string[2:5]) #prints third letter till fifth excluding the sixth one
