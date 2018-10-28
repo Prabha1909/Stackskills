3c91aaf498b31d618ace409e32757fad127c90ae
Prabha1909 <prabha.svm@gmail.com>
2018-10-28 19:33:26 +0530

    print vs return in function

---
+++ Basics/12_functions.py
@@ -0,0 +1,9 @@
+def calculate_age(year):
+    age = 2018 - year
+    return age
+
+print(calculate_age(1990))
+
+#diff between return and print
+#while using pring we can reuse the output in the code. It returns the type of the object depends on the output
+#whereas using the print is allowed to reuse the output in the code. It returns the None type.
