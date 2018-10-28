756fbebdb7e7a1e8921233ef549fb59cde6b756f
Prabha1909 <prabha.svm@gmail.com>
2018-10-28 13:26:09 +0530

    count method

--- Basics/7_list_methods.py
+++ Basics/7_list_methods.py
@@ -1,9 +1,15 @@
-address = ["address1", "address2", "address3"]
+address = ["address1", "address2", "address3", "address1", "address2"]
+#append method
 address.append("India")
 print(address)
+#remove method
 address.remove("address2")
 print(address)
+#pop method
 my_country = address.pop(-1)
 print(my_country)
+#copy method
 copy_of_address = address.copy()
 print(copy_of_address)
+#count method
+print(address.count("address1"))
