82b8d10f4c38515751f620d3f0b62a8542a48540
Prabha1909 <prabha.svm@gmail.com>
2018-10-28 21:30:46 +0530

    splitlines method is used on string output of text file and made it a list containing string

--- Basics/13_working_with_files.py
+++ Basics/13_working_with_files.py
@@ -1,4 +1,11 @@
-my_file = open("sample.txt")
+my_file = open("files/sample.txt")
 fruits = my_file.read()
 my_file.close()
 print(fruits)
+myfile = open("files/sample.txt")
+content = myfile.read()
+print(content)
+content = content.splitlines()   #returns a list
+print(content)
+
+#we can use seek method to bring back the cursor position to 0
