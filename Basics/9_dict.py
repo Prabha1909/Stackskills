b80b25de061fc2b0a4ef6a2a3a9dfe544dc3c4ad
Prabha1909 <prabha.svm@gmail.com>
2018-10-28 17:32:13 +0530

    dict py created

---
+++ Basics/9_dict.py
@@ -0,0 +1,9 @@
+my_dict = {"Prabha":"19091990", "Saira": "29072000", "Sachin": "06052018"}
+
+print(my_dict.keys())
+print(my_dict.values())
+print(my_dict.items())
+my_son = my_dict.popitem()
+print(my_son)
+print(type(my_son))
+print(type(my_dict.keys()))
