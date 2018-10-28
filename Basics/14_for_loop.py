0d103f7ce1f36a6622b21dd5254968a6eaa8231f
Prabha1909 <prabha.svm@gmail.com>
2018-10-28 22:36:28 +0530

    basics.py changes in the for_loop and Beyond_basic folder with while.py created

---
+++ Basics/14_for_loop.py
@@ -0,0 +1,12 @@
+mylist = [1, 2, 3]
+
+for item in mylist:
+    print("num is {}".format(item))
+
+mydict = {"Mike":1234, "John":1111}
+
+for k,v in mydict.items():
+    print(v)
+
+for letter in ["Hello", 2]:
+    print(letter)
