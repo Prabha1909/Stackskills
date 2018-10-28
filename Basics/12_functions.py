e2dd962afc477512b5f214a5e117cb88b2f36912
Prabha1909 <prabha.svm@gmail.com>
2018-10-28 19:53:01 +0530

    function with more than one parameter and also with default parameter

--- Basics/12_functions.py
+++ Basics/12_functions.py
@@ -7,3 +7,11 @@ print(calculate_age(1990))
 #diff between return and print
 #while using pring we can reuse the output in the code. It returns the type of the object depends on the output
 #whereas using the print is allowed to reuse the output in the code. It returns the None type.
+
+
+
+#Functions with more than one parameter
+def km_to_m(km, coefficient = 0.6214):
+    return km * coefficient
+
+print(km_to_m(10))
