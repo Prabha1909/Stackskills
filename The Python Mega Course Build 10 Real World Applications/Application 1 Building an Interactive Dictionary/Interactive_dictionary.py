369589f439b6efcecf94b860c6a08c2a39f36840
Prabha1909 <prabha.svm@gmail.com>
2018-10-31 08:22:45 +0530

    implementing case sensitivity

--- The Python Mega Course Build 10 Real World Applications/Application 1 Building an Interactive Dictionary/Interactive_dictionary.py
+++ The Python Mega Course Build 10 Real World Applications/Application 1 Building an Interactive Dictionary/Interactive_dictionary.py
@@ -2,13 +2,15 @@ import json
 
 data = json.load(open("C:\\Users\\pselvam\\Desktop\\Python_Tutorial\\data.json"))
 
-def meaning(word):
-    if word in data:
-        return data[word]
+def meaning(w):
+    w = w.lower()
+    if w in data:
+        return data[w]
     else:
         return "The word is not in the dictionary"
 
 
 word = input("Enter the word: ")
+
 
 print(meaning(word))
