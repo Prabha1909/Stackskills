c97f1cd4a6a0cabf1e24b92d488625e858c3c98f
Prabha1909 <prabha.svm@gmail.com>
2018-10-31 08:43:46 +0530

    recommending the best match

--- The Python Mega Course Build 10 Real World Applications/Application 1 Building an Interactive Dictionary/Interactive_dictionary.py
+++ The Python Mega Course Build 10 Real World Applications/Application 1 Building an Interactive Dictionary/Interactive_dictionary.py
@@ -1,16 +1,16 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("C:\\Users\\pselvam\\Desktop\\Python_Tutorial\\data.json"))
 
 def meaning(w):
     w = w.lower()
+    w = input("Did you mean {}? enter y for yes and n for No".format(get_close_matches(w, data.keys())[0]))
     if w in data:
         return data[w]
     else:
         return "The word is not in the dictionary"
 
-
 word = input("Enter the word: ")
-
 
 print(meaning(word))
