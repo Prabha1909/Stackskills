662f416cba1443db2d8a7b296e3577912e3bcf86
Prabha1909 <prabha.svm@gmail.com>
2018-10-31 08:17:24 +0530

    getting a message for non-existing words

--- The Python Mega Course Build 10 Real World Applications/Application 1 Building an Interactive Dictionary/Interactive_dictionary.py
+++ The Python Mega Course Build 10 Real World Applications/Application 1 Building an Interactive Dictionary/Interactive_dictionary.py
@@ -3,6 +3,12 @@ import json
 data = json.load(open("C:\\Users\\pselvam\\Desktop\\Python_Tutorial\\data.json"))
 
 def meaning(word):
-    return data[word]
+    if word in data:
+        return data[word]
+    else:
+        return "The word is not in the dictionary"
 
-print(meaning("rain"))
+
+word = input("Enter the word: ")
+
+print(meaning(word))
