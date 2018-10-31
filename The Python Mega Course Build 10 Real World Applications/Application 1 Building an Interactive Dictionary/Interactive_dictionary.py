f6d4d18d6554030c707e30040445bc25e9367138
Prabha1909 <prabha.svm@gmail.com>
2018-10-31 21:42:39 +0530

    added code for Delhi and USA type of inputs

--- The Python Mega Course Build 10 Real World Applications/Application 1 Building an Interactive Dictionary/Interactive_dictionary.py
+++ The Python Mega Course Build 10 Real World Applications/Application 1 Building an Interactive Dictionary/Interactive_dictionary.py
@@ -7,6 +7,10 @@ def meaning(w):
     w = w.lower()
     if w in data:
         return data[w]
+    elif w.title() in data:
+        return data[w.title()]
+    elif w.upper() in data:
+        return data[w.upper()]
     elif len(get_close_matches(w, data.keys(), cutoff=0.8)) > 0:
         user_input = input("Did you mean {} instead? Enter Y if Yes and N if No".format(get_close_matches(w, data.keys(), cutoff=0.8)[0]))
         user_input = user_input.upper()
@@ -21,4 +25,9 @@ def meaning(w):
 
 word = input("Enter the word: ")
 
-print(meaning(word))
+output = meaning(word)
+if type(output) == list:
+    for items in output:
+        print(items)
+else:
+    print(output)
