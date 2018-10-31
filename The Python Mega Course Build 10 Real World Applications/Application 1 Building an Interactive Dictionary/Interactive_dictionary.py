fec1e12c0ae8d051de8b0589802eb4acb7ee0315
Prabha1909 <prabha.svm@gmail.com>
2018-10-31 08:00:12 +0530

    loading json using import json package

--- The Python Mega Course Build 10 Real World Applications/Application 1 Building an Interactive Dictionary/Interactive_dictionary.py
+++ The Python Mega Course Build 10 Real World Applications/Application 1 Building an Interactive Dictionary/Interactive_dictionary.py
@@ -2,11 +2,4 @@ import json
 
 data = json.load(open("C:\\Users\\pselvam\\Desktop\\Python_Tutorial\\data.json"))
 
-def meaning():
-    word = input("Enter the word: ")
-    if word in data:
-        return data[word.lower()]
-    else:
-        return "The word is not in the dictionary"
-
-print(meaning())
+print(data["rain"])
