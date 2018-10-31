847d3ed4ae0db095a5be70d075c01f0096a9d458
Prabha1909 <prabha.svm@gmail.com>
2018-10-31 07:55:07 +0530

    my version of the program before going through the lecture

--- The Python Mega Course Build 10 Real World Applications/Application 1 Building an Interactive Dictionary/Interactive_dictionary.py
+++ The Python Mega Course Build 10 Real World Applications/Application 1 Building an Interactive Dictionary/Interactive_dictionary.py
@@ -2,4 +2,11 @@ import json
 
 data = json.load(open("C:\\Users\\pselvam\\Desktop\\Python_Tutorial\\data.json"))
 
-print(data["rain"])
+def meaning():
+    word = input("Enter the word: ")
+    if word in data:
+        return data[word.lower()]
+    else:
+        return "The word is not in the dictionary"
+
+print(meaning())
